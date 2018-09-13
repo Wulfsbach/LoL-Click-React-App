28f5a69f16300ef42a02cf27b99ffac32732e067
Wulfsbach <etemelkos@gmail.com>
2018-09-13 13:33:27 -0400

    app almost there

--- src/assets/clicky/clicky.js
+++ src/assets/clicky/clicky.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, {Component} from "react";
 import logos from "../../logos.json";
 import "./clicky.css";
 
 
+class clicky extends Component  {
+    render() {
+        return (
+            <div className="container">
+            <logosGrid />
+            </div>
+        );
+    }
+        
+
+}
+
+class logosGrid extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state ={
+            logos: logos,
+            guessed: [],
+            highscore:0
+
+        };
+    }
+    shuffle() {
+for (let i = 0; i<this.state.logos.length;i++){
+    const j = Math.floor(Math.random()* (i + 1));
+ [this.state.logos[i],this.state.logos[j]]=[this.state.logos[j],this.state.logos[i]];
+}
+    }
+
+End() {
+    if (this.state.highscore >= 10) {
+        this.setState({highscore: this.state.logos, logos:logos, guessed:[]});
+    }else {
+        this.setState({logos:logos, guessed:[]});
+    }
+}
+
+Checker(a) { 
+    let choice= true;
+    this.state.guessed.forEach((guess) => {
+        if (choice === guess) {
+            check = false;
+        }
+    });
+    return check;
+}
+
+
+}
