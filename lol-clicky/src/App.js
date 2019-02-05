b331ae8a4b1e86b8d018defd5c2bdf0b2eefe951
Wulfsbach <etemelkos@gmail.com>
2019-02-05 18:23:00 -0500

    Deleted files that had no effect on the program itself and made the lol-clicky folder contents the primary files in the repo

--- lol-clicky/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Clicky from './assets/Clicky/clicky';
-import Heading from './assets/Heading/heading';
-
-
-const App = () => <div> <Heading /> <Clicky /> </div>
-
-export default App;
